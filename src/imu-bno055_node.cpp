9b792c2bc71548b2120b255ac0772c55ad94cef7
Christianjc <christianjc_09@yahoo.com>
2022-02-10 17:50:18 -0800

    wello world node working in c++

---
+++ src/imu-bno055_node.cpp
@@ -0,0 +1,39 @@
+/**
+ *  MIT License
+ *  Copyright (c) 2021 Christian Castaneda <github.com/christianjc>
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ * 
+*/
+
+#include <ros/ros.h>
+
+int main(int argc, char **argv) {
+
+    /** This initializes the ros client library and gives the node a default
+        name: imu_bno055_node **/
+    ros::init(argc, argv, "imu_bno055_node");
+
+    /** Making the nodehandle object registers this node with the master node **/
+    ros::NodeHandle nh;
+
+    /** Prints a string to the INFO log **/
+    ROS_INFO_STREAM("Hello world imu node");
+
+}
