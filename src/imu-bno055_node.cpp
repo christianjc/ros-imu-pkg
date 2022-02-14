ded0ac396115dd33f42a7a99eb777adeaf527fee
Christianjc <christianjc_09@yahoo.com>
2022-02-14 14:17:23 -0800

    added and tested to bno055 sensors to the ros node and it works. One displays raw data and the other fusion data

--- src/imu-bno055_node.cpp
+++ src/imu-bno055_node.cpp
@@ -37,15 +37,15 @@ int main(int argc, char **argv) {
 
     /** Making the nodehandle object registers this node with the master node **/
     ros::NodeHandle nh_fusion;
-    // ros::NodeHandle nh_raw;
+    ros::NodeHandle nh_raw;
 
     /* Set topic name and queue size */
     ros::Publisher pub_bno_fusion = nh_fusion.advertise<ros_imu_pkg::Fusion_imu>("imu_fusion", 10, true);
-    // ros::Publisher pub_bno_raw = nh_raw.advertise<ros_imu_pkg::Raw_imu>("imu_raw", 10, true);
+    ros::Publisher pub_bno_raw = nh_raw.advertise<ros_imu_pkg::Raw_imu>("imu_raw", 10, true);
 
     /* Declare imu msg type */
     ros_imu_pkg::Fusion_imu imu_fusion;
-    // ros_imu_pkg::Raw_imu imu_raw;
+    ros_imu_pkg::Raw_imu imu_raw;
 
     /* Set the loop rate */
     ros::Rate rate(3);
@@ -54,15 +54,15 @@ int main(int argc, char **argv) {
     ROS_INFO_STREAM("Starting Node...");
 
     bno055_imu::BNO055Driver node_fusion("/dev/i2c-1", 0x28, bno055_imu::OPERATION_MODE_IMUPLUS);
-    // bno055_imu::BNO055Driver node_raw("/dev/i2c-1", 0x28, bno055_imu::OPERATION_MODE_ACCGYRO);
+    bno055_imu::BNO055Driver node_raw("/dev/i2c-1", 0x29, bno055_imu::OPERATION_MODE_ACCGYRO);
     node_fusion.init();
-    // node_raw.init();
+    node_raw.init();
 
     while(ros::ok()) {
         node_fusion.read_imu_data(imu_fusion);
-        // node_raw.read_imu_data_raw(imu_raw);
+        node_raw.read_imu_data_raw(imu_raw);
         pub_bno_fusion.publish(imu_fusion);
-        // pub_bno_raw.publish(imu_raw);
+        pub_bno_raw.publish(imu_raw);
         rate.sleep();
     }
 }
